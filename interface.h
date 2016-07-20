bc2084c667e6dbb737f9e053018e133cbafda1c7
thcmatias <thcmatias@gmail.com>
2016-07-20 19:44:35 -0300

    * implementing menuRetiraProc

--- interface.h
+++ interface.h
@@ -6,6 +6,7 @@
 void comoUsar (char* progName, char* argumentos);
 void menuPrincipal ();
 void menuInsereProc();
+void menuRetiraProc();
 void printaProcessos();
 void printaEspacosLivres ();
 void testaAlocacaoDeSegmentosNaMemoria ();
--- processo.c
+++ processo.c
@@ -13,7 +13,7 @@ PROCESSO* criaProcesso(int pid, int numSeg, int* tamanhos) {
     }
     temp->pid = pid;
     temp->numSeg = numSeg;
-    if (!(temp->segTable = calloc(numSeg,sizeof(LINHA))))
+    if (!(temp->segTable = calloc(numSeg, sizeof(LINHA))))
     {
         printf("funcao criaProcesso: Erro ao alocar memoria do temp->segTable com calloc\n");
         exit(-1);
